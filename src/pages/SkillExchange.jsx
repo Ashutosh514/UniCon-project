67f93c6cf8d7316ce21b4ccd65f93b569a5ddb49
ASHUTOSH KUMAR <ashutoshfzl12@gmail.com>
2026-01-03 15:08:55 +0530

    Update content policy checkbox change handler
    Refactor checkbox change handler to directly modify state variable.

--- src/pages/SkillExchange.jsx
+++ src/pages/SkillExchange.jsx
@@ -335,7 +335,7 @@ const SkillExchange = () => {
             <div className="border-t border-gray-700 pt-4"> 
               <div className="flex items-start space-x-3"> 
                 <input type="checkbox" id="contentPolicy" checked={contentPolicyAccepted} 
-                  onChange={(e) => setContentPolicyAccepted(e.target.checked)} 
+                  onChange={(e) => (contentPolicyAccepted = e.target.checked)} 
                   className="mt-1 h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" required /> 
                 <div className="flex-1"> <label htmlFor="contentPolicy" className="text-sm text-gray-300"> 
                   I agree to the{' '} <button type="button" onClick={() => setShowContentPolicy(true)} 
